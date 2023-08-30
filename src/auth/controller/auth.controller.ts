d4db5d38bcb25e3f9149e34b70b2a36f610472df
dlsxody1 <dlsxody1@naver.com>
2023-08-30 07:34:11 +0000

    update

--- src/auth/controller/auth.controller.ts
+++ src/auth/controller/auth.controller.ts
@@ -38,7 +38,7 @@ export class AuthController {
     @Body() request: UpdateUserRequest,
   ): Promise<SuccessResponse> {
     //TODO : accessTOken type설정
-    console.log(request, '업데이트를 한다.');
+    console.log(request, '업데이트를 한다 ..');
     return this.userService.updateUser(id, request);
   }
   // Todo: 처음에 유저정보만 가지고 create -> register채워서 update
