3939d05a0dfb78de0ec12d5808d2ab74cc65bfac
baek <bgh9651@gmail.com>
2023-10-30 17:36:27 +0900

    Fix. config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       script: 'dist/main.js',
       exec_mode: 'cluster',
       instances: '1',
-      node_args: '-r ts-node/register', // 수정 필요
+      // node_args: '-r ts-node/register', // 수정 필요
       // 또는
       // node_args: '-r <ts-node-path>', // 수정 필요
     },
